8273c636d6126bf5ec826c066d8a41a6ebec68d8
Kavinder Singh <kavinder.singh.3702@gmail.com>
2023-09-26 23:46:02 +0530

    passing values as parameters

--- Code/file_handling.py
+++ Code/file_handling.py
@@ -1,41 +1,51 @@
-def printps(self):
-    openfile = None
-    ftypes = [('postscript files', '*.ps'), ('ALL files', '*')]
-    openfile = asksaveasfilename(filetypes=ftypes)
-    if openfile:
-        outfile = os.path.abspath(openfile)
-        self.ipostscript(outfile)
+import os
+import pickle
+from tkinter.filedialog import asksaveasfilename, askopenfilename
+
+class CADFileHandler:
+    def __init__(self, initial_canvas=None, initial_filename=None, initial_curr=None):
+        self.canvas = initial_canvas if initial_canvas is not None else None
+        self.filename = initial_filename if initial_filename is not None else None
+        self.curr = initial_curr if initial_curr is not None else {}
+
+    def printps(self):
+        openfile = None
+        ftypes = [('postscript files', '*.ps'), ('ALL files', '*')]
+        openfile = asksaveasfilename(filetypes=ftypes)
+        if openfile:
+            outfile = os.path.abspath(openfile)
+            self.ipostscript(outfile)
 
 
-def ipostscript(self, file='drawing.ps'):
-    ps = self.canvas.postscript()
-    ps = ps.replace('1.000 1.000 1.000 setrgbcolor',
-                    '0.000 0.000 0.000 setrgbcolor')
-    fd = open(file, 'w')
-    fd.write(ps)
-    fd.close()
+    def ipostscript(self, file='drawing.ps'):
+        ps = self.canvas.postscript()
+        ps = ps.replace('1.000 1.000 1.000 setrgbcolor',
+                        '0.000 0.000 0.000 setrgbcolor')
+        fd = open(file, 'w')
+        fd.write(ps)
+        fd.close()
 
 
-def fileopen(self):
-    openfile = None
-    ftypes = [('cadvas dwg', '*.pkl'), ('ALL FILES', '*')]
-    openfile = askopenfilename(filetypes=ftypes, defaultextension='.pkl')
-    if openfile:
-        infile = os.path.abspath(openfile)
-        self.load(infile)
-        defaultextension
+    def fileopen(self):
+        openfile = None
+        ftypes = [('cadvas dwg', '*.pkl'), ('ALL FILES', '*')]
+        openfile = askopenfilename(filetypes=ftypes, defaultextension='.pkl')
+        if openfile:
+            infile = os.path.abspath(openfile)
+            self.load(infile)
+            defaultextension
 
 
-def fileImport(self):
-    openfile = None
-    ftypes = [('DXF format', '*.dxf'), ('ALL FIles', '*')]
-    openfile = askopenfilename(filetypes=ftypes, defaultextension='.dxf')
-    if openfile:
-        infile = os.path.abspath(openfile)
-        self.load(infile)
+    def fileImport(self):
+        openfile = None
+        ftypes = [('DXF format', '*.dxf'), ('ALL FIles', '*')]
+        openfile = askopenfilename(filetypes=ftypes, defaultextension='.dxf')
+        if openfile:
+            infile = os.path.abspath(openfile)
+            self.load(infile)
 
 
-def filesave(self):
+    def filesave(self):
     openfile = self.filename
     if openfile:
         outfile = os.path.abspath(openfile)
