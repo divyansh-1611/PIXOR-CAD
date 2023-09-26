a138d4edcc660ffa634fa833589c686cab4494a4
bhardwajj03 <adi.bhardwaj09@gmail.com>
2023-09-27 00:15:38 +0530

    Tk Reverse Polish Notation

--- Code/RPNcalculator.py
+++ Code/RPNcalculator.py
@@ -95,5 +95,136 @@ class cal(Toplevel):
         button(self, 12, 6, 'ln', lambda op='math.log(x)': self.func(op), span=3, clr='darkgreen')
         button(self, 12, 9, 'log', lambda op='math.log10(x)': self.func(op), span=3, clr='darkgreen')
 
+        def quit(self):
+            if self.caller:
+                self.caller.calculator = None
+            self.destroy()
 
+        def pr(self, val):
+            """Send value in register to CADvas."""
+            # There must be a better way to get this value
+            str_value = repr(eval('self.' + val + 'display.get()')).strip("'")
+            self.caller.enterfloat(str_value)
+            self.keip = False
+            self.needrup = True
+
+        def keyin(self, c):
+            if self.keip:
+                self.xdisplay.set(self.xdisplay.get() + c)
+            else:
+                self.keip = True
+                if self.needrup:
+                    self.rotateup(loop=0)
+                self.clearx()
+                self.keyin(c)
+
+        def enter(self):
+            self.tdisplay.set(self.zdisplay.get())
+            self.zdisplay.set(self.ydisplay.get())
+            self.ydisplay.set(self.xdisplay.get())
+            self.keip = False
+            self.needrup = False
+
+        def calc(self, op):
+            pass
+            """Arithmetic calculations between x and y registers, then rotate down."""
+            try:
+                if op == '+/-':
+                    self.xdisplay.set(repr(eval('-' + self.xdisplay.get())))
+                else:
+                    x = repr(eval(self.ydisplay.get() + op + self.xdisplay.get()))
+                    self.xdisplay.set(x)
+                    self.ydisplay.set(self.zdisplay.get())
+                    self.zdisplay.set(self.tdisplay.get())
+                self.keip = False
+                self.needrup = True
+            except:
+                self.xdisplay.set("ERROR")
+
+        def func(self, op, in_cnvrt=0, out_cnvrt=0):
+            """Evaluate function op then put result in x-register, don't rotate stack.
+            if in_cnvrt: convert input value of x-register from degrees to radians.
+            if out_cnvrt: convert output value of x-register from radians to degrees."""
+            try:
+                x = float(self.xdisplay.get())
+            except:
+                x = 0
+            try:
+                y = float(self.ydisplay.get())
+            except:
+                y = 0
+            if in_cnvrt:
+                x = x * math.pi / 180
+            result = eval(op)
+            if out_cnvrt:
+                result = result * 180 / math.pi
+            self.xdisplay.set(f2s(result))
+            self.keip = False
+            self.needrup = True
+
+        def mm2in(self):
+            if self.xdisplay.get():
+                self.xdisplay.set(repr(eval(self.xdisplay.get() + '/25.4')))
+                self.keip = False
+                self.needrup = True
+
+        def in2mm(self):
+            if self.xdisplay.get():
+                self.xdisplay.set(repr(eval(self.xdisplay.get() + '*25.4')))
+                self.keip = False
+                self.needrup = True
+
+        def storex(self):
+            self.mem = self.xdisplay.get()
+            self.keip = False
+            self.needrup = True
+
+        def recallx(self):
+            self.rotateup()
+            self.xdisplay.set(self.mem)
+            self.keip = False
+            self.needrup = True
+
+        def rotateup(self, loop=1):
+            x = self.tdisplay.get()
+            self.tdisplay.set(self.zdisplay.get())
+            self.zdisplay.set(self.ydisplay.get())
+            self.ydisplay.set(self.xdisplay.get())
+            if loop:
+                self.xdisplay.set(x)
+
+        def rotatedn(self):
+            x = self.xdisplay.get()
+            self.xdisplay.set(self.ydisplay.get())
+            self.ydisplay.set(self.zdisplay.get())
+            self.zdisplay.set(self.tdisplay.get())
+            self.tdisplay.set(x)
+
+        def trimx(self):
+            self.xdisplay.set(self.xdisplay.get()[:-1])
+
+        def swapxy(self):
+            x = self.xdisplay.get()
+            y = self.ydisplay.get()
+            self.xdisplay.set(y)
+            self.ydisplay.set(x)
+
+        def clearx(self):
+            self.xdisplay.set('')
+
+        def clearall(self):
+            self.xdisplay.set('')
+            self.ydisplay.set('')
+            self.zdisplay.set('')
+            self.tdisplay.set('')
+
+        def putx(self, value):
+            if self.needrup:
+                self.rotateup(loop=0)
+            self.xdisplay.set(repr(value))
+            self.keip = False
+            self.needrup = True
+
+    if __name__ == '__main__':
+        Cal().mainloop()
 
